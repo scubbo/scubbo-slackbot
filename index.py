cd5804eb73101e97b0b300e2b9861e8a1ef3e3eb
Jack Jackson <scubbojj@gmail.com>
2017-11-02 00:34:50 -0700

    Reinstate Netrunner

--- index.py
+++ index.py
@@ -1,35 +1,47 @@
 # -*- coding: utf-8 -*-
-import os
+import os, re
 import urllib, urllib2
 from json import loads, dumps
+
+REGEX = re.compile('\[\[(.*?)\]\]')
+ALL_CARDS = loads(urllib2.urlopen('https://netrunnerdb.com/api/2.0/public/cards').read())['data']
+
+with file('abbreviations.json', 'r') as f:
+  ABBREVIATIONS = loads(f.read())
 
 def handler(event, context):
   text = event['event']['text']
   channel = event['event']['channel']
   message_id = event['event']['ts']
 
-  if text.startswith('Hey Bot!'):
-    print 'Found a match!'
-    submitResponse(channel, message_id)
+  matches = REGEX.findall(text)
 
-  # Gotta return *something* from this method...
+  if matches:
+    print 'matches was some'
+    for match in matches:
+      print 'working on ' + str(match)
+      if match in ABBREVIATIONS:
+        print 'match was in ABBREVIATIONS'
+        match = ABBREVIATIONS[match]
+      print 'checking for ' + match
+      for card in ALL_CARDS:
+        if all([c in card['title'].lower() for c in match.lower().split()]):
+          print 'all condition was matched'
+          submitResponse(card['code'], card['title'], channel, message_id)
+          break
+  else:
+    print 'matches was None'
   return {
     "statusCode": 200,
     "headers": {"Content-Type":"application/json"},
-    "body": "I hear ya"
+    "body": 'I hear ya'
   }
 
-def submitResponse(channel, original_message_id):
-  url = 'https://slack.com/api/chat.postMessage'
-  data = {
-    'token': os.environ['responseToken'],
-    'attachments': urllib.quote_plus(dumps([{
-      'image_url': 'https://img.buzzfeed.com/buzzfeed-static/static/2014-01/campaign_images/webdr06/7/14/50-reasons-why-nicolas-cage-is-the-greatest-human-1-5571-1389124720-1_big.jpg',
-      'title': 'Hey chum! Here\'s a picture of Nicolas Cage!'
-    }])),
-    'channel': channel,
-    'thread_ts': original_message_id
-  }
-  data_as_string = '&'.join([item[0] + '=' + item[1] for item in data.items()])
-  req = urllib2.Request(url, data_as_string)
-  urllib2.urlopen(req)
+def submitResponse(card_id, card_name, channel, original_message_id):
+    print 'inside submitResponse: ' + str(card_id) + ':' + str(card_name) + ':' + str(channel)
+    url = 'https://slack.com/api/chat.postMessage'
+    
+    data = 'token=' + os.environ['responseToken'] + '&attachments=%5B%7B%27image_url%27%3A+%27https%3A%2F%2Fnetrunnerdb.com%2Fcard_image%2F' + card_id + '.png%27%2C+%27title%27%3A+%27' + card_name + '%27%7D%5D&channel=' + channel + '&thread_ts=' + original_message_id
+    print 'data: ' + str(data)
+    req = urllib2.Request(url, data)
+    urllib2.urlopen(req)
