359112f47ed500215af1f764f0f7063d9addd8b0
Jack Jackson <scubbojj@gmail.com>
2018-02-26 23:27:51 -0800

    Introduce CoinCoinHandler
    Needs some work, obviously!
    
    Query https://coinmarketcap.com/api/ for currency values/existence.

---
+++ lib/CoinCoinHandler.py
@@ -0,0 +1,17 @@
+class CoinCoinHandler(object):
+
+  self.REGEX = re.compile('.*(\S+coin).*')
+  self.SC = SlackClient(os.environ['responseToken'])
+
+  def can_handle(self, event):
+    text = event['event']['text']
+    match = self.REGEX.match(text)
+    if match and event['event']['user'] == 'U1S0XJP0W':
+      return (True, match.group(1))
+    else:
+      return (False,)
+
+  def handle(self, event, match_context):
+    channel = event['event']['channel']
+    message_id = event['event']['ts']
+    self.SC.send_threaded_reply(self, channel, message_id, "I see you want to know about " + match_context)
